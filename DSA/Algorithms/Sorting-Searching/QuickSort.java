d31d209e89b46821a16107411ac1705da66f879a
Tharun Chowdary <65231374+Tharun0102@users.noreply.github.com>
2020-10-04 18:01:42 +0530

    Add files via upload

---
+++ DSA/Algorithms/Sorting-Searching/QuickSort.java
@@ -0,0 +1,47 @@
+    private static void quicksort(int[] arr,int start,int end){
+
+    	if(start>=end) return;
+
+    	int pivot=arr[start];
+
+    	int left=start+1,right=end;
+
+    	while(left<=right){
+
+    		if(arr[left]>pivot && arr[right]<pivot){
+
+    			swap(arr,left,right);
+
+    		}
+  
+  		if(arr[left]<=pivot){
+
+    			left++;
+
+    		}
+
+    		if(arr[right]>=pivot){
+
+    			right--;
+
+    		}
+
+    	}
+
+    	swap(arr,start,right);
+
+    	quicksort(arr,start,right);
+
+    	quicksort(arr,right+1,end);
+
+    }
+    
+private static void swap(int[] arr,int i,int j){
+
+    	int temp=arr[i];
+
+        arr[i]=arr[j];
+
+        arr[j]=temp;
+
+    }
