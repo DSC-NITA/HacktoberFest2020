8c8ebe3742722b3c7967c7a7aeecba65a7c4f3a6
RoyalTechie <geniusdigvijaysing@gmail.com>
2020-10-25 11:06:58 +0530

    Retry

--- DSA/Algorithms/Sorting-Searching/Sorting/ListSort.py
+++
@@ -1,9 +0,0 @@
-#Python code for list sorting  (List Sort can Sort both Int & Char)
-
-input_string = input("Enter a list elements separated by space: ")
-print("\n")         
-userList = input_string.split()
-#Click Enter to Check Result
-print("user list is: ", userList)
-userList.sort()
-print("Sorted List is: ",userList)
