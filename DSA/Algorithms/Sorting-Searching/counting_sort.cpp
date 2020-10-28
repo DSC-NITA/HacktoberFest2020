95f0068d1684f52ddd768f677269ff9fc103c831
shrishtisrivastava <shrishtisrivastava04@gmail.com>
2020-10-28 09:48:03 +0530

    Counting sort algorithm

---
+++ DSA/Algorithms/Sorting-Searching/counting_sort.cpp
@@ -0,0 +1,90 @@
+/*.....counting sort algorithm.*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// the countingSort function below.
+vector<int> countingSort(vector<int> arr) {
+ array<int, 100> count;
+    count.fill(0);
+    for (int i=0;i<arr.size();i++) {
+        ++count[arr[i]];
+    }
+    vector<int> cVec(count.begin(), count.end());
+    vector<int> res;
+    for (int i=0;i<cVec.size();i++)  {
+        while(--cVec[i]>=0) {
+            res.push_back(i);
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    vector<int> result = countingSort(arr);
+
+    for (int i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << " ";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
